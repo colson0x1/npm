beffb47ab2aa5f3545102451d3c7ffa87760fc5a
Colson <colsoncalifornication@gmail.com>
2024-03-16 13:57:39 +0545

    improve project structure

---
+++ src/api/queries/searchPackages.ts
@@ -0,0 +1,4 @@
+/* Contains a function to make the actual API request */
+import type { PackageSummary } from '../types/packageSummary';
+
+export async function searchPackages(): Promise<PackageSummary[]> {}
---
+++ src/api/types/packageSummary.ts
@@ -0,0 +1,7 @@
+/* Defines the 'PackageSummary' interface */
+export interface PackageSummary {
+  name: string;
+  version: string;
+  description: string;
+  keywords: string[];
+}
