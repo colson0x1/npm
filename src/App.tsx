c31893683a75adf8fa07d08888fbfcc19549e783
Colson <colsoncalifornication@gmail.com>
2024-03-16 08:10:00 +0545

    implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './pages/Root';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import DetailsPage from './pages/DetailsPage';
+
+/* createBrowserRouter is like an object which is going to look at the
+ * current path of the user is visiting and decide on which of these 3
+ * different components to display
+ */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/search',
+        element: <SearchPage />,
+      },
+      {
+        path: '/packages/:name',
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className='text-red-600'>NPM</div>;
+  /*
+   * Router Provider thing is like the same Provider from the Context system.
+   * Behind the scenes, react router dom uses the context system to
+   * communicate what page user is currently navigating to and lot of state
+   * around navigation to the rest of our app
+   * We wrap our entire application using this Router Provider thing and we're
+   * gonna provide our router as a prop to it
+   */
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Root.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+
+/* Outlet from react-router-dom is a React Component and it reaches up to the
+ * Browser Router through the context system and says, hey what is the currently
+ * selected component. Which of the page that we're currently supposed to
+ * display. Browser Router will tell which to display it and the Outlet will
+ * simply display it.
+ */
+
+export default function Root() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
